cd7e99e7581418fcb02330bf07bf32691355c4f2
Dushyanth <93592455+Dushyanthyadav@users.noreply.github.com>
2026-08-24 17:55:43 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // priority_queue<double, vector<double> std::greater<double>> minHeap;
+        map<double, vector<vector<int>>> distanceMap;
+
+        for (vector<int> point: points) {
+            int x = point[0];
+            int y = point[1];
+            double distance = sqrt(x*x + y*y);
+            distanceMap[distance].push_back({x, y});
+        }
+
+        vector<vector<int>> result;
+        for (auto &[dist, point]: distanceMap) {
+            for (auto num: point) {
+                result.push_back(num);
+                k--;
+                if(k<=0) {
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+};
